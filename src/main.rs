a6f7f0e5d51a707390036f3f4cecc9e859ea2de8
Diana Lin <lindiana1206@gmail.com>
2025-08-10 00:22:18 -0400

    add another servo

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use core::sync::atomic::{AtomicI32, Ordering};
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::OutputType;
-use embassy_stm32::peripherals::{TIM1, TIM10};
+use embassy_stm32::peripherals::{TIM1, TIM2};
 use embassy_stm32::time::hz;
 use embassy_stm32::timer::Channel;
 use embassy_stm32::timer::simple_pwm::{PwmPin, SimplePwm};
@@ -15,18 +15,24 @@ use embassy_time::Duration;
 use {defmt_rtt as _, panic_probe as _};
 
 type SimplePwmMutex = Mutex<CriticalSectionRawMutex, Option<SimplePwm<'static, TIM1>>>;
+// type SimplePwmMutex = Mutex<CriticalSectionRawMutex, Option<SimplePwm<'static, TIM2>>>;
 static PWM: SimplePwmMutex = Mutex::new(None);
 static SERVO_POSITION: AtomicI32 = AtomicI32::new(0); // Initial position in angle (-90 to 90 degrees)
+static SERVO_POSITION_2: AtomicI32 = AtomicI32::new(0); // Initial position for second servo
 
 #[embassy_executor::task(pool_size = 4)]
-async fn servo_control(pwm: &'static SimplePwmMutex, channel: Channel) {
+async fn servo_control(
+    pwm: &'static SimplePwmMutex,
+    channel: Channel,
+    servo_position: &'static AtomicI32,
+) {
     if let Some(pwm_ref) = pwm.lock().await.as_mut() {
         pwm_ref.channel(channel).enable();
         info!("Servo control started!");
     }
 
     loop {
-        let pos = SERVO_POSITION.load(Ordering::Relaxed);
+        let pos = servo_position.load(Ordering::Relaxed);
 
         if let Some(pwm_ref) = pwm.lock().await.as_mut() {
             let duty_cycle = ((pos + 90 + 45) as u32) * (pwm_ref.max_duty_cycle() as u32) / 1800;
@@ -50,12 +56,13 @@ async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
     let servo_pin = PwmPin::new_ch1(p.PE9, OutputType::PushPull); // D6
+    let servo_pin_2 = PwmPin::new_ch2(p.PA9, OutputType::PushPull);
 
     // Initialize the PWM channel for the servo
     let pwm = SimplePwm::new(
         p.TIM1,
         Some(servo_pin),
-        None,
+        Some(servo_pin_2),
         None,
         None,
         hz(50), // 50 Hz for servo control
@@ -66,12 +73,18 @@ async fn main(spawner: Spawner) {
         *(PWM.lock().await) = Some(pwm);
     }
 
-    spawner.spawn(servo_control(&PWM, Channel::Ch1)).unwrap();
+    spawner
+        .spawn(servo_control(&PWM, Channel::Ch1, &SERVO_POSITION))
+        .unwrap();
+    spawner
+        .spawn(servo_control(&PWM, Channel::Ch2, &SERVO_POSITION_2))
+        .unwrap();
 
     loop {
         // Simulate changing the servo position
         for pos in (-90..=90).step_by(45) {
             SERVO_POSITION.store(pos, Ordering::Relaxed);
+            SERVO_POSITION_2.store(-pos, Ordering::Relaxed);
             embassy_time::Timer::after(Duration::from_millis(1000)).await; // Wait 1 second between changes
         }
 
